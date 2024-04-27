3e20dd2a9ab20a41d9d17afa0c6c9ae3cd4d91bf
marufjonovshoxrux <159701524+marufjonovshoxrux@users.noreply.github.com>
2024-04-27 13:21:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let alphabet_reversed = ""
+
+let alphabet_arr = [
+   
+]
+
+for (let i = alphabet.length + 1; i >= 0; i--) {
+    alphabet_arr.push(alphabet[i]);
+}
+
+console.log(alphabet_arr); 
+
+
